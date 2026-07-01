e2fab2be54bc12c43e0912e1d75e476271f3bf8a
Valentin Efimov <vefimov@gifts.ru>
2026-07-01 13:41:28 +0300

    fix add_service.bat

--- add_service.bat
+++ add_service.bat
@@ -1,7 +1,7 @@
 @echo off
-set SrvcName="1C:Enterprise 8.3 RAS"
-set BinPath="C:\Program Files\1cv8\8.3.17.1851\bin\ras.exe cluster --service"
-set Desctiption="1C:Enterprise 8.3 RAS"
+set SrvcName="1C:Enterprise RAS"
+set BinPath="C:\Program Files\1cv8\8.3.27.1688\bin\ras.exe cluster --service --port=1545"
+set Desctiption="1C:Enterprise RAS"
 sc stop %SrvcName%
 sc delete %SrvcName%
 sc create %SrvcName% binPath= %BinPath% start= auto displayname= %Desctiption%
